f70188924dff82fd475206bb8ca9db5991b3a47f
Siam48444 <siam48444@gmail.com>
2023-12-21 16:34:10 +0600

    tt

--- index.js
+++ index.js
@@ -11,17 +11,32 @@ gsap.ticker.lagSmoothing(0);
 // hero load animations.
 var tl = gsap.timeline();
 
+tl.to("#loader h3", {
+   opacity: 0,
+   duration: 0.4,
+   delay: 0.3,
+   ease: "power1.inOut",
+});
+tl.to("#loader", {
+   y: "-100%",
+   pointerEvents: "none",
+   duration: 1.2,
+   ease: "power1.inOut",
+});
 tl.from(".load_appear_texts", {
    y: "100%",
-   duration: 0.7,
+   duration: 0.5,
+   ease: "power1.inOut",
    stagger: 0.3,
 });
 tl.to(".hero_image_containers_shutters", {
    y: "-100%",
-   duration: 0.7,
+   duration: 0.5,
+   ease: "power1.inOut",
 });
 tl.from("nav, .hero_bottom", {
    scale: 0.95,
    opacity: 0,
-   duration: 0.7,
+   duration: 0.5,
+   ease: "power1.inOut",
 });

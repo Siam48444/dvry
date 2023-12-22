5794dee9b7151ce0995a392dc02be0a81daaee98
Siam48444 <siam48444@gmail.com>
2023-12-22 17:22:41 +0600

    kk

--- index.js
+++ index.js
@@ -12,12 +12,23 @@ gsap.ticker.lagSmoothing(0);
 const hamburgerBtn = document.querySelector("#hamburger_button");
 hamburgerBtn.addEventListener("click", () => {
    var hamburgerAtt = hamburgerBtn.getAttribute("data-hamburger-click");
+   var menu = document.querySelector(".menu");
 
    if (hamburgerAtt === "false") {
       hamburgerBtn.setAttribute("data-hamburger-click", "true");
+      gsap.to(menu, {
+         y: 0,
+         duration: 0.6,
+         ease: "power1.inOut",
+      });
    }
    if (hamburgerAtt === "true") {
       hamburgerBtn.setAttribute("data-hamburger-click", "false");
+      gsap.to(menu, {
+         y: "-100%",
+         duration: 0.6,
+         ease: "power1.inOut",
+      });
    }
 });
 
